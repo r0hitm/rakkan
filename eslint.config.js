6d94ce8dada8589a97f84949df154e67b7e1e085
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-06-02 00:12:23 +0530

    Add eslint + prettier

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist/*'],
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+]);
